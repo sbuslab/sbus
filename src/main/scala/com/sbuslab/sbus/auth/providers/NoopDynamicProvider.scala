eba03a5020fefc59f201d5d36d7a362615b29b96
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-11-03 14:52:36 +0000

    INF-502 enable sbus auth on live optional (#21)
    * update jackson to latest 2.13.4
    
    * INF-502 enable sbus auth on live optional

--- src/main/scala/com/sbuslab/sbus/auth/providers/NoopDynamicProvider.scala
+++ src/main/scala/com/sbuslab/sbus/auth/providers/NoopDynamicProvider.scala
@@ -4,12 +4,14 @@ import net.i2p.crypto.eddsa.EdDSAPublicKey
 
 import com.sbuslab.sbus.auth.{Action, DynamicAuthConfigProvider, Identity}
 
+
 class NoopDynamicProvider() extends DynamicAuthConfigProvider {
+
   override def getPublicKeys: Map[String, EdDSAPublicKey] = Map.empty
 
   override def getActions: Map[String, Action] = Map.empty
 
   override def getIdentities: Map[String, Identity] = Map.empty
 
-  override def isRequired = Option.empty
+  override def isRequired = None
 }
